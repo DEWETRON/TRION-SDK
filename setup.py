74e6b8113e92243cda00c6fa2ff2bf649f69d995
Matthias Straka <matthias.straka@dewetron.com>
2024-10-09 14:08:46 +0200

    python: remove usupported versions

--- setup.py
+++ setup.py
@@ -27,9 +27,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -42,5 +39,5 @@ setuptools.setup(
     platforms=["Windows", "Linux"],
     packages=["trion_sdk"],
     package_dir={"trion_sdk": "trion_api/python"},
-    python_requires=">=3.6",
+    python_requires=">=3.9",
 )
