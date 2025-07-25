013609ad1aa1c49dd41c08d9d4e7c15233f0aa24
Gunther Laure <gunther.laure@gmail.com>
2025-07-25 09:47:30 +0200

    TRION-SDK-UI: initial wizard generated .NET MAUI example

---
+++ app/TRION-SDK-UI/TRION-SDK-UI.iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+using Foundation;
+
+namespace TRION_SDK_UI.iOS
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ app/TRION-SDK-UI/TRION-SDK-UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TRION_SDK_UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
