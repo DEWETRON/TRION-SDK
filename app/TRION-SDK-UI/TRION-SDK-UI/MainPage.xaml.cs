388a0050f1ef5e313e80335d8e2e4560a3032bd7
Gunther Laure <gunther.laure@gmail.com>
2025-07-25 13:10:00 +0200

    TRION-SDK-UI: Add simple sidebar and log view

--- app/TRION-SDK-UI/TRION-SDK-UI/MainPage.xaml.cs
+++ app/TRION-SDK-UI/TRION-SDK-UI/MainPage.xaml.cs
@@ -1,25 +1,50 @@
-﻿namespace TRION_SDK_UI
+﻿using System.Collections.ObjectModel;
+
+namespace TRION_SDK_UI
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        public ObservableCollection<string> ChannelNames { get; } = new();
+        public ObservableCollection<string> LogMessages { get; } = new();
+
+        double _startX;
+        double _startWidth;
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+
+            ChannelNames.Add("Channel 1");
+            ChannelNames.Add("Channel 2");
+            ChannelNames.Add("Channel 3");
+
+            LogMessages.Add("App started.");
+
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnDragHandlePanUpdated;
+            DragHandle.GestureRecognizers.Add(panGesture);
+        }
+
+        private void OnDragHandlePanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    _startWidth = SidebarColumn.Width.Value;
+                    break;
+                case GestureStatus.Running:
+                    double newWidth = _startWidth - e.TotalX;
+                    if (newWidth < 100) newWidth = 100;
+                    if (newWidth > 400) newWidth = 400;
+                    SidebarColumn.Width = new GridLength(newWidth);
+                    break;
+            }
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            LogMessages.Add($"Button clicked at {DateTime.Now:T}");
         }
     }
-
 }
