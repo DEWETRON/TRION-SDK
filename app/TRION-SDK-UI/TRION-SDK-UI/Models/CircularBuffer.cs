341846b0bf6f232253c378b05adf75147b5c2e4a
Bernhard Pregetter <bernhard.pregetter@dewetron.com>
2025-10-06 14:59:35 +0200

    added comments for CircularBuffer

--- app/TRION-SDK-UI/TRION-SDK-UI/Models/CircularBuffer.cs
+++ app/TRION-SDK-UI/TRION-SDK-UI/Models/CircularBuffer.cs
@@ -1,17 +1,31 @@
-﻿using HarfBuzzSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Trion;
 
 namespace TRION_SDK_UI.Models
 {
+    /// <summary>
+    /// Lightweight snapshot of the first acquisition circular buffer
+    /// associated with a TRION board.
+    /// </summary>
+    /// <remarks>
+    /// Values are captured at construction time:
+    /// - EndPosition: Physical (device) end pointer of buffer 0.
+    /// - Size: Total allocated memory size of buffer 0 (bytes).
+    /// This class does not poll for updates; create a new instance to refresh.
+    /// </remarks>
     internal class CircularBuffer(int board_id)
     {
-        public long StartPosition { get; set; } = TrionApi.DeWeGetParam_i64(board_id, Trion.TrionCommand.BUFFER_0_START_POINTER).value;
+        /// <summary>
+        /// Hardware-reported absolute end pointer (address) of buffer 0.
+        /// Useful for wrap‑around calculations when interpreting DMA offsets.
+        /// </summary>
+        public long EndPosition { get; set; } =
+            TrionApi.DeWeGetParam_i64(board_id, TrionCommand.BUFFER_0_END_POINTER).value;
 
-        public long EndPosition { get; set; } = TrionApi.DeWeGetParam_i64(board_id, Trion.TrionCommand.BUFFER_0_END_POINTER).value;
-        public int Size { get; set; } = TrionApi.DeWeGetParam_i32(board_id, Trion.TrionCommand.BUFFER_0_TOTAL_MEM_SIZE).value;
+        /// <summary>
+        /// Total memory size (in bytes) of buffer 0. Can be used to modulo
+        /// pointer arithmetic when walking samples in a ring layout.
+        /// </summary>
+        public int Size { get; set; } =
+            TrionApi.DeWeGetParam_i32(board_id, TrionCommand.BUFFER_0_TOTAL_MEM_SIZE).value;
     }
 }
