e84abd5dc9e51e330f72a0f9bd4c8b6626e5440c
Bernhard Pregetter <bernhard.pregetter@dewetron.com>
2026-01-27 14:00:04 +0100

    moved circularbuffer into AcquisitionManager

--- app/TRION-SDK-UI/TRION-SDK-UI/Services/AcquisitionManager.cs
+++ app/TRION-SDK-UI/TRION-SDK-UI/Services/AcquisitionManager.cs
@@ -8,6 +8,35 @@ using TRION_SDK_UI.POCO;
 using TrionApiUtils;
 
 namespace TRION_SDK_UI.Services;
+
+internal sealed class CircularBuffer
+{
+    public long EndPosition { get; }
+    public int Size { get; }
+    public long StartPosition { get; }
+
+    public CircularBuffer(int board_id)
+    {
+        var (err, endPos) = TrionApi.DeWeGetParam_i64(board_id, TrionCommand.BUFFER_0_END_POINTER);
+        Utils.CheckErrorCode(err, "Failed to get buffer end pointer");
+        EndPosition = endPos;
+
+        (err, var size) = TrionApi.DeWeGetParam_i32(board_id, TrionCommand.BUFFER_0_TOTAL_MEM_SIZE);
+        Utils.CheckErrorCode(err, "Failed to get buffer total mem size");
+        Size = size;
+
+        StartPosition = EndPosition - Size;
+    }
+
+    public void CheckWrapAround(ref long readPos)
+    {
+        if (readPos >= EndPosition)
+        {
+            readPos -= Size;
+        }
+    }
+}
+
 public class AcquisitionManager(Enclosure enclosure)
 {
     private readonly Enclosure _enclosure = enclosure;
